701d10f4ec616be2c9f61b94c8a72c0c2638824d
Kyle <kcparks1234@gmail.com>
2023-11-07 13:04:10 -0800

    Centered the navlinks

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,35 +4,24 @@ import './home.module.scss';
 const Home: React.FC = () => {
     return (
         <div className="home">
-
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Kyle Parks Portfolio</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarText">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">About</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Projects</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Contact</a>
-                            </li>
-                        </ul>
-                        <a className="navbar-text" href="#">
-                            Resume
-                        </a>
-                    </div>
+            <nav className="navbar navbar-expand-lg">
+                <div className="collapse navbar-collapse" id="navbarText">
+                    <ul className="navbar-nav mx-auto">
+                        <li className="nav-item text-center mx-3">
+                            <a className="nav-link active fs-5" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item text-center mx-3">
+                            <a className="nav-link fs-5" href="#">About</a>
+                        </li>
+                        <li className="nav-item text-center mx-3">
+                            <a className="nav-link fs-5" href="#">Projects</a>
+                        </li>
+                        <li className="nav-item text-center mx-3">
+                            <a className="nav-link fs-5" href="#">Contact</a>
+                        </li>
+                    </ul>
                 </div>
             </nav>
-
         </div>
     );
 };
