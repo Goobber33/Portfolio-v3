c468f6d8b77015cec427f88062b6b7693ff30222
Kyle <kcparks1234@gmail.com>
2023-11-15 16:57:39 -0800

    Added custom styling to my home section.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import styles from './home.module.scss';
 
 const Home: React.FC = () => {
+    const textRef = useRef<HTMLDivElement>(null);
+
     return (
-        <div className="position-relative vh-100">
-            <div className="position-absolute" style={{ top: '25%', left: '50%', transform: 'translate(-50%, -30%)', fontFamily: "'DM Serif Display', serif" }}>
-                <h1 className="text-center" style={{ color: '#00056B', fontSize: '8rem' }}>Hi. I'm Kyle</h1>
-                <p className="text-center" style={{ color: '#00056B', fontSize: '2rem' }}>I'm a Software Engineer</p>
+        <div className={`d-flex align-items-start justify-content-center vh-100 ${styles.animateText}`} style={{ paddingTop: '10%' }}>
+            <div 
+                className="text-center" 
+                style={{ animation: 'slideUp 1s ease forwards' }}
+                ref={textRef}
+            >
+                <h1 style={{ color: '#00056B', fontSize: '8rem', fontFamily: "'DM Serif Display', serif" }}>Hi. I'm Kyle</h1>
+                <p style={{ color: '#00056B', fontSize: '2.5rem', fontFamily: "'DM Serif Display', serif" }}>I'm a Software Engineer</p>
+                <div className="mt-5 p-4" style={{ maxWidth: '600px', margin: 'auto', fontFamily: "'Roboto', serif" }}>
+                    <p style={{ color: '#00056B', fontSize: '1.5rem' }}>
+                        Passionate about building seamless digital experiences with clean and efficient code.
+                    </p>
+                </div>
             </div>
         </div>
     );
